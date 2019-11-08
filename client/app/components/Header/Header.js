c4e5bbfa6ec805c2a0cf76310890a4ff8c50d833
F. Rick Reich <frickreich@gmail.com>
2019-11-08 14:38:45 +0100

    Add new header
    Fix #2

--- client/app/components/Header/Header.js
+++ client/app/components/Header/Header.js
@@ -8,10 +8,8 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import CloseIcon from '@material-ui/icons/Close';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 import IconButton from '@material-ui/core/IconButton';
-
 import DialogWindow from '../DialogWindow/DialogWindow';
 
 const styles = theme => ({
@@ -38,6 +36,7 @@ class Header extends Component
 
         this.state =
         {
+            token: '',
             showLogin: false
         }
     }
@@ -60,14 +59,11 @@ class Header extends Component
             showLogin: false
         })
     }
- 
-    /**
-     * @todo Replace temporary menu
-     * @body The temporary header menu needs to be replaced with a material-ui app-bar.
-     */
+
     render()
     {
         const { classes } = this.props;
+        const { token } = this.state;
 
         return (
             <div className={classes.header}>
@@ -76,9 +72,20 @@ class Header extends Component
                         <Typography variant="h6" className={classes.title}>
                             history.day
                         </Typography>
-                        <Button color="inherit" onClick={ () => this.openLogin() }>
-                            Login
-                        </Button>
+                        {
+                            token ?
+                            <IconButton
+                                edge="start"
+                                color="inherit"
+                                aria-label="close"
+                            >
+                                <AccountCircle />
+                            </IconButton>
+                            :
+                            <Button color="inherit" onClick={ () => this.openLogin() }>
+                                Login/Register
+                            </Button>
+                        }
                     </Toolbar>
                 </AppBar>
 
@@ -102,39 +109,22 @@ class Header extends Component
 export default withStyles(styles)(Header);
 
 /*
-
-<Dialog fullScreen open={ this.state.showLogin } onClose={ this.closeLogin.bind(this) } >
-                    <AppBar position="fixed">
-                    <Toolbar>
-                        <IconButton edge="start" color="inherit" onClick={ this.closeLogin.bind(this) } aria-label="close">
-                            <CloseIcon />
-                        </IconButton>
-                        <Typography variant="h6" className={ classes.dialogTitle }>
-                            Login
-                        </Typography>
-                        </Toolbar>
-                    </AppBar>
-                </Dialog>
-
-
-*/
-/*
 <header>
-                <nav id="topMenu">
-                    <ul>
-                        <li>
-                            <NavLink exact to="/" activeClassName="active">Home</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/login" activeClassName="active">Login</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/entries" activeClassName="active">Entries</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/newEntry" activeClassName="active">New Entry</NavLink>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
+    <nav id="topMenu">
+        <ul>
+            <li>
+                <NavLink exact to="/" activeClassName="active">Home</NavLink>
+            </li>
+            <li>
+                <NavLink to="/login" activeClassName="active">Login</NavLink>
+            </li>
+            <li>
+                <NavLink to="/entries" activeClassName="active">Entries</NavLink>
+            </li>
+            <li>
+                <NavLink to="/newEntry" activeClassName="active">New Entry</NavLink>
+            </li>
+        </ul>
+    </nav>
+</header>
 */
